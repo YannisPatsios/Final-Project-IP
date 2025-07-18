ce44eedad826454a8afd3e91e246ae35bfb649f9
YannisPatsios <yannispatsios@hotmail.com>
2025-07-19 02:01:51 +0300

    Actual first deployment
    I should have enabled * allowed hosts in settings.py for deployment in all hosting services. both python anywhere and render

--- sdr_world/settings.py
+++ sdr_world/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-h%%3k-+61&5^30_uzm$dj0%%0bw6)1%v6gmggo+1hl#4(gcuo0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
