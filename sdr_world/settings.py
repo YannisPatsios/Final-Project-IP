0876741811104f85cfbbeef40a11f710a3dee661
YannisPatsios <yannispatsios@hotmail.com>
2025-07-18 12:24:38 +0300

    v1.1
    turned debug off

--- sdr_world/settings.py
+++ sdr_world/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h%%3k-+61&5^30_uzm$dj0%%0bw6)1%v6gmggo+1hl#4(gcuo0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
